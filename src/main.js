16a4be76142f129e9440d4bf00b215b67cd62523
Mido <elm042025@gmail.com>
2025-08-06 17:25:42 +0200

    installed npm package and configured its options

--- src/main.js
+++ src/main.js
@@ -1,24 +1,34 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import WebGLFluid from "webgl-fluid";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+WebGLFluid(document.querySelector("canvas"), {
+   TRIGGER: "hover",
+   IMMEDIATE: true,
+   AUTO: false,
+   INTERVAL: 3000,
+   SIM_RESOLUTION: 256,
+   DYE_RESOLUTION: 1024,
+   CAPTURE_RESOLUTION: 512,
+   DENSITY_DISSIPATION: 1,
+   VELOCITY_DISSIPATION: 0.2,
+   PRESSURE: 0.8,
+   PRESSURE_ITERATIONS: 20,
+   CURL: 30,
+   SPLAT_RADIUS: 0.1,
+   SPLAT_FORCE: 6000,
+   SPLAT_COUNT: Number.parseInt(Math.random() * 20) + 5,
+   SHADING: true,
+   COLORFUL: true,
+   COLOR_UPDATE_SPEED: 10,
+   PAUSED: false,
+   BACK_COLOR: { r: 0, g: 0, b: 0 },
+   TRANSPARENT: false,
+   BLOOM: false,
+   BLOOM_ITERATIONS: 8,
+   BLOOM_RESOLUTION: 256,
+   BLOOM_INTENSITY: 0.8,
+   BLOOM_THRESHOLD: 0.6,
+   BLOOM_SOFT_KNEE: 0.7,
+   SUNRAYS: true,
+   SUNRAYS_RESOLUTION: 196,
+   SUNRAYS_WEIGHT: 1.0,
+});
